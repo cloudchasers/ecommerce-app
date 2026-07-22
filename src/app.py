074900d0ad75b70124ad8160bfb03973c98f005f
Neo Bernabe <48178671+NeonPikachu17@users.noreply.github.com>
2026-07-22 17:06:57 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -63,4 +63,4 @@ def checkout(product_id):
     return render_template('checkout.html', product=product, qr_url=qr_url)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
